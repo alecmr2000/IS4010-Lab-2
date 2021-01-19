a9c50af32d763fc0866a172038b0a0405f732adb
alecmr2000 <alecmr2000@gmail.com>
2021-01-19 20:14:43 +0000

    Half-way point.

--- Main.java
+++ Main.java
@@ -1,5 +1,37 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("M&M Color Counts");
+
+    int total;
+    double blue, brown, green, orange, red, yellow;
+
+    total = 55 * 11;
+    //Above is accounting for the 11 bags we are experimenting with.
+
+    blue = total * .24;
+    brown = total * .13;
+    green = total * .16;
+    orange = total * .20;
+    red = total * .13;
+    yellow = total * .14;
+    //Above is adding in color distribution expected in each bag.
+
+    System.out.println("Blue: " + blue);
+    System.out.println("Brown: " + brown);
+    System.out.println("Green: " + green);
+    System.out.println("Orange: " + orange);
+    System.out.println("Red: " + red);
+    System.out.println("Yellow: " + yellow);
+    //Above is displaying the data with appropriate lables.
+
+    double checktotal;
+    checktotal = blue + brown + green + orange + red + yellow;
+    System.out.println("Total: " + checktotal);
+    //Above creates a variable to check the percentages match up to the number of M&Ms.
+
+
+    
+
+
   }
 }
